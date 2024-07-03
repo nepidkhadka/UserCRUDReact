fa2c19dea45d54b535f6abc8a0a14652f7117bcd
nepidkhadka <dipenkhadka191@gmail.com>
2024-07-03 13:29:04 +0545

    Form Validation Part Completed

---
+++ src/page/Home.jsx
@@ -0,0 +1,362 @@
+import { useFormik } from "formik";
+import React, { useEffect, useRef, useState } from "react";
+import { FormValidation } from "../utils/FormValidation";
+
+const Home = () => {
+  const [userData, setUserData] = useState({});
+  const [countries, setCountries] = useState([]);
+  const [formSubmitted, setFormSubmitted] = useState(false); // Track form submission
+
+  const fullNameRef = useRef();
+
+  // Formik Form Validation
+  const { values, handleBlur, handleChange, handleSubmit, errors, setFieldValue, resetForm } = useFormik({
+    initialValues: {
+      fullname: "",
+      email: "",
+      phonenumber: "",
+      dob: "",
+      city: "",
+      district: "",
+      province: "",
+      country: "Nepal",
+      image: null,
+    },
+    validationSchema: FormValidation,
+    onSubmit: (values) => {
+      setUserData(values);
+      setFormSubmitted(true); 
+      console.log(values);
+    },
+  });
+
+  // AutoFocus To FullName Input
+  useEffect(() => {
+    fullNameRef.current.focus();
+  }, []);
+
+  // Fetch countries data from API
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((response) => response.json())
+      .then((data) => {
+        const countryNames = data.map((country) => country.name.common);
+        setCountries(countryNames);
+      })
+      .catch((error) => console.error("Error fetching countries:", error));
+  }, []);
+
+// Reset form values when formSubmitted state changes
+   useEffect(() => {
+    if (formSubmitted) {
+        resetForm(); 
+        setFormSubmitted(false); 
+    }
+    }, [formSubmitted, resetForm]);
+
+  return (
+    <div className="container mx-auto">
+      <div className="p-6 rounded-md ">
+        {/* Heading */}
+        <h2 className="text-3xl font-bold">Add User</h2>
+        <p className="text-lg">Fill the form to add users</p>
+
+        {/* User Form */}
+        <form className="mt-8 grid grid-cols-6 gap-3 bg-[#2f2f2f] p-6 rounded-md" onSubmit={handleSubmit}>
+            
+        {/* Full Name */}
+        <div className="col-span-6 sm:col-span-3">
+        <label
+            htmlFor="fullname"
+            className="block text-sm font-medium text-white dark:text-gray-200"
+        >
+            Full Name :
+        </label>
+
+        <input
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.fullname}
+            ref={fullNameRef}
+            type="text"
+            id="fullname"
+            name="fullname"
+            placeholder="eg. Jane Copper"
+            className="mt-2 w-full rounded-md h-10 p-4 outline-none border-[#7a7a7a] bg-inherit border"
+        />
+        <br />
+        {errors.fullname && (
+            <small className="text-red-500 p-2 font-medium">
+            {errors.fullname}
+            </small>
+        )}
+        <br />
+        </div>
+
+        {/* Email */}
+        <div className="col-span-6 sm:col-span-3">
+        <label
+            htmlFor="email"
+            className="block text-sm font-medium text-white dark:text-gray-200"
+        >
+            Email :
+        </label>
+
+        <input
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.email}
+            type="email"
+            id="email"
+            name="email"
+            placeholder="eg. info@treeleaf.ai"
+            className="mt-2 w-full rounded-md h-10 p-4 outline-none border-[#7a7a7a] bg-inherit border"
+        />
+        <br />
+        {errors.email && (
+            <small className="text-red-500 p-2 font-medium">
+            {errors.email}
+            </small>
+        )}
+        <br />
+        </div>
+
+        {/* Phone Number */}
+        <div className="col-span-6 sm:col-span-3">
+        <label
+            htmlFor="phonenumber"
+            className="block text-sm font-medium text-white dark:text-gray-200"
+        >
+            Phone Number :
+        </label>
+
+        <input
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.phonenumber}
+            type="text"
+            id="phonenumber"
+            name="phonenumber"
+            placeholder="eg. 9876543210"
+            className="mt-2 w-full rounded-md h-10 p-4 outline-none border-[#7a7a7a] bg-inherit border"
+        />
+        <br />
+        {errors.phonenumber && (
+            <small className="text-red-500 p-2 font-medium">
+            {errors.phonenumber}
+            </small>
+        )}
+        <br />
+        </div>
+
+        {/* Date Of Birth */}
+        <div className="col-span-6 sm:col-span-3">
+        <label
+            htmlFor="dob"
+            className="block text-sm font-medium text-white dark:text-gray-200"
+        >
+            Date of Birth :
+        </label>
+
+        <input
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.dob}
+            type="date"
+            id="dob"
+            name="dob"
+            className="mt-2 w-full rounded-md h-10 p-4 outline-none border-[#7a7a7a] bg-inherit border"
+        />
+        <br />
+        {errors.dob && (
+            <small className="text-red-500 p-2 font-medium">
+            {errors.dob}
+            </small>
+        )}
+        <br />
+        </div>
+
+        {/* City */}
+        <div className="col-span-6 sm:col-span-3">
+        <label
+            htmlFor="city"
+            className="block text-sm font-medium text-white dark:text-gray-200"
+        >
+            City :
+        </label>
+
+        <input
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.city}
+            type="text"
+            id="city"
+            name="city"
+            placeholder="eg. Lubhu"
+            className="mt-2 w-full rounded-md h-10 p-4 outline-none border-[#7a7a7a] bg-inherit border"
+        />
+        <br />
+        {errors.city && (
+            <small className="text-red-500 p-2 font-medium">
+            {errors.city}
+            </small>
+        )}
+        <br />
+        </div>
+
+        {/* District */}
+        <div className="col-span-6 sm:col-span-3">
+        <label
+            htmlFor="district"
+            className="block text-sm font-medium text-white dark:text-gray-200"
+        >
+            District :
+        </label>
+
+        <input
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.district}
+            type="text"
+            id="district"
+            name="district"
+            placeholder="eg. Lalitpur"
+            className="mt-2 w-full rounded-md h-10 p-4 outline-none border-[#7a7a7a] bg-inherit border"
+        />
+        <br />
+        {errors.district && (
+            <small className="text-red-500 p-2 font-medium">
+            {errors.district}
+            </small>
+        )}
+        <br />
+        </div>
+
+        {/* Province */}
+        <div className="col-span-6 sm:col-span-3">
+        <label
+            htmlFor="province"
+            className="block text-sm font-medium text-white dark:text-gray-200"
+        >
+            Province :
+        </label>
+
+        <select
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.province}
+            id="province"
+            name="province"
+            className="mt-2 w-full rounded-md h-10 p-2 outline-none dark:text-white border-[#7a7a7a] bg-inherit border"
+        >
+            <option className="bg-[#252d3c]" value="Madhesh">
+            Province No. 1 (Madhesh)
+            </option>
+            <option className="bg-[#252d3c]" value="Bagmati">
+            Province No. 2 (Bagmati Province)
+            </option>
+            <option className="bg-[#252d3c]" value="Lumbini">
+            Province No. 3 (Lumbini Province)
+            </option>
+            <option className="bg-[#252d3c]" value="Koshi">
+            Province No. 4 (Koshi Province)
+            </option>
+            <option className="bg-[#252d3c]" value="Sudurpashchim">
+            Province No. 5 (Sudurpashchim Province)
+            </option>
+            <option className="bg-[#252d3c]" value="Gandaki">
+            Province No. 6 (Gandaki Province)
+            </option>
+            <option className="bg-[#252d3c]" value="Karnali">
+            Province No. 7 (Karnali Province)
+            </option>
+        </select>
+        <br />
+        {errors.province && (
+            <small className="text-red-500 p-2 font-medium">
+            {errors.province}
+            </small>
+        )}
+        <br />
+        </div>
+
+        {/* Country */}
+        <div className="col-span-6 sm:col-span-3">
+        <label
+            htmlFor="country"
+            className="block text-sm font-medium text-white dark:text-gray-200"
+        >
+            Country :
+        </label>
+
+        <select
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.country}
+            name="country"
+            id="country"
+            className="mt-2 w-full rounded-md h-10 p-2 outline-none dark:text-white border-[#7a7a7a] bg-inherit border"
+        >
+            <option className="bg-[#252d3c]" value="Nepal">
+            Nepal
+            </option>
+            {countries &&
+            countries.map((country, index) => (
+                <option key={index} value={country} className="bg-[#252d3c]">
+                {country}
+                </option>
+            ))}
+        </select>
+        <br />
+        {errors.country && (
+            <small className="text-red-500 p-2 font-medium">
+            {errors.country}
+            </small>
+        )}
+        <br />
+        </div>
+
+        {/* Image */}
+        <div className="col-span-6 sm:col-span-3">
+        <label
+            htmlFor="image"
+            className="block text-sm font-medium text-gray-700 dark:text-gray-200"
+        >
+            Image :
+        </label>
+
+        <input
+            type="file"
+            id="image"
+            name="image"
+            onChange={(event) => {
+            setFieldValue("image", event.currentTarget.files[0]);
+            }}
+            onBlur={handleBlur}
+            className="mt-2 w-full rounded-md h-10 p-2 outline-none border-[#252d3c] bg-inherit border"
+        />
+        <br />
+        {errors.image && (
+            <small className="text-red-500 p-2 font-medium">
+            {errors.image}
+            </small>
+        )}
+        <br />
+        </div>
+
+        {/* Action Button */}
+        <div className="col-span-6">
+        <button
+            type="submit"
+            className="mt-4 w-full bg-green-500 text-white rounded-md h-10 hover:opacity-80"
+        >
+            Add User
+        </button>
+        </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/utils/FormValidation.jsx
@@ -0,0 +1,25 @@
+import * as Yup from "yup";
+
+export const FormValidation = Yup.object().shape({
+  fullname: Yup.string()
+    .matches(/^[a-zA-Z ]+$/, "Full Name must contain only letters and spaces")
+    .required("Full Name is required"),
+  email: Yup.string()
+    .email("Invalid email address")
+    .required("Email is required"),
+  phonenumber: Yup.string()
+    .matches(/^\d{7,10}$/, "Phone number must be between 7 to 10 digits")
+    .required("Phone Number is required"),
+  dob: Yup.string().required("Date of Birth is required"),
+  city: Yup.string().required("City is required"),
+  district: Yup.string().required("District is required"),
+  province: Yup.string().required("Province is required"),
+  country: Yup.string().required("Country is required"),
+  image: Yup.mixed()
+    .required("Image is required")
+    .test(
+      "fileFormat",
+      "Unsupported Format. Only PNG files are allowed.",
+      (value) => value && value.type === "image/png"
+    ),
+});
